92c302326611f36c90e720e5fbf8658e1cbe32c2
Adithya Nair <adithyanair121@gmail.com>
2025-12-05 02:37:49 +0530

    Special token splitting

--- assignment1-basics/cs336_basics/pretokenizater.py
+++ assignment1-basics/cs336_basics/pretokenizater.py
@@ -2,45 +2,8 @@ import os
 from abc import ABC
 from dataclasses import dataclass
 from typing import BinaryIO
-
 import regex as re  # ty:ignore[unresolved-import]
-
-# Taken from the handout
-class Tokenizer(ABC):
-    """Abstract interface for a tokenizer."""
-
-    def encode(self, string: str) -> list[int]:
-        raise NotImplementedError
-
-    def decode(self, indices: list[int]) -> str:
-        raise NotImplementedError
-
-
-@dataclass(frozen=True)
-class BPETokenizerParams:
-    """All you need to specify a BPETokenizer."""
-
-    vocab: dict[int, bytes]  # index -> bytes
-    merges: dict[tuple[int, int], int]  # index1,index2 -> new_index
-
-
-class BPETokenizer(Tokenizer):
-    """BPE tokenizer given a set of merges and a vocabulary."""
-
-    def __init__(self, params: BPETokenizerParams):
-        self.params = params
-
-    def encode(self, string: str) -> list[int]:
-        indices = list(map(int, string.encode("utf-8")))
-        for pair, new_index in self.params.merges.items():
-            indices = merge(indices, pair, new_index)
-        return indices
-
-    def decode(self, indices: list[int]) -> str:
-        bytes_list = list(map(self.params.vocab.get, indices))
-        string = b"".join(bytes_list).decode("utf-8")  # @inspect string
-        return string
-
+from multiprocessing import Pool
 
 def merge(
     indices: list[int], pair: tuple[int, int], new_index: int
@@ -60,40 +23,6 @@ def merge(
             new_indices.append(indices[i])
             i += 1
     return new_indices
-
-
-def train_bpe_pretokenization(
-    count_map: dict[str, int], num_merges: int
-) -> BPETokenizerParams:
-    pretoken_sequences: dict[str, list[int]] = {}
-    for pretoken in count_map:
-        pretoken_sequences[pretoken] = list(pretoken.encode("utf-8"))
-
-    merges: dict[tuple[int, int], int] = {}
-    vocab: dict[int, bytes] = {x: bytes([x]) for x in range(256)}
-    for i in range(num_merges):
-        counts: dict[tuple[int, int], int] = {}
-
-        for pretoken, indices in pretoken_sequences.items():
-            for index1, index2 in zip(indices, indices[1:]):
-                counts[(index1, index2)] = (
-                    counts.get((index1, index2), 0) + count_map[pretoken]
-                )
-                pair = max(counts, key=counts.get)  # ty:ignore[no-matching-overload]
-                index1, index2 = pair
-                new_index = 256 + i
-                merges[pair] = new_index
-                vocab[new_index] = (
-                    vocab[index1] + vocab[index2]
-                )  # @inspect vocab
-
-        for pretoken in pretoken_sequences:
-            pretoken_sequences[pretoken] = merge(
-                pretoken_sequences[pretoken], pair, new_index
-            )
-
-    return BPETokenizerParams(vocab=vocab, merges=merges)
-
 
 def find_chunk_boundaries(
     file: BinaryIO,
@@ -143,40 +72,63 @@ def find_chunk_boundaries(
     # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
     return sorted(set(chunk_boundaries))
 
-from multiprocessing import Pool
 
-input_path = "notes.org"
-
-def process_chunk(start: int, end: int) -> dict[str, int]:
+def chunk_processer(start: int, end: int, input_path: str, special_tokens: list[str]):
     with open(input_path, "rb") as f:
         f.seek(start)
-        chunk = f.read(end - start).decode("utf-8", errors="ignore")
-        # Run pre-tokenization on your chunk and store the counts for each pre-token
+        file_chunk = f.read(end - start).decode("utf-8", errors="ignore")
+        escaped_special_tokens: list[str] = [re.escape(token) for token in special_tokens]
+        special_token_pattern = "|".join(escaped_special_tokens)
+        split_chunk_list: list[str] = re.split(f"({special_token_pattern})", file_chunk)
+        # GPT-2 regex
         PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
         count_map: dict[str, int] = {}
-        # Pre-tokenization
-        pretokens: list[str] = re.finditer(PAT, chunk)
-        for item in pretokens:
-            count_map[item[0]] = count_map.get(item[0], 0) + 1
-        return count_map
+        for chunk in split_chunk_list:
+            pretokens = re.finditer(PAT, chunk)
+            for item in pretokens:
+                count_map[item.group(0)] = count_map.get(item.group(0), 0) + 1
+    return count_map
 
 def run_train_bpe(input_path: str, vocab_size: int, special_tokens: list[str]):
-        num_processes = 8
+        num_processes = 32
+        final_count_map: dict[str, int]= {}
         with open(input_path, mode="rb") as f:
             boundaries: list[int] = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
         with Pool(num_processes) as pool:
-            count_maps: list[dict[str,int]] = pool.starmap(process_chunk, [i for i in zip(boundaries[:-1], boundaries[1:])])
-            final_count_map: dict[str, int]= {}
+            count_maps: list[dict[str,int]] = pool.starmap(chunk_processer,[(start, end, input_path, special_tokens) for start, end in zip(boundaries[:-1], boundaries[1:])])
             for map in count_maps:
                 for pretoken in map:
                     final_count_map[pretoken] = final_count_map.get(pretoken, 0) + map[pretoken]
-        params = train_bpe_pretokenization(final_count_map, 300)
 
+        def train_bpe_pretokenization(count_map: dict[str, int], num_merges: int):
+            merges: dict[tuple[int, int], int] = {}
+            vocab: dict[int, bytes] = {x: bytes([x]) for x in range(256)}
 
-string = "notes.org"
-vocab_size = 200
-special_tokens = ["<|end_of_text|>"]
+            special_map: dict[str, int] = {}
+            for i, special_token in enumerate(special_tokens):
+                byte_special = special_token.encode('utf-8')
+                vocab[256+i] = byte_special
+                special_map[special_token] = 256 + i
+            pretoken_sequences: dict[str, list[int]] = {}
+            for pretoken in count_map:
+                if pretoken in special_tokens:
+                    reserved_index = special_map[pretoken]
+                    pretoken_sequences[pretoken] = [reserved_index]
+                else:
+                    pretoken_sequences[pretoken] = list(pretoken.encode("utf-8"))
+            for i in range(num_merges):
+                counts: dict[tuple[int, int], int] = {}
+                for pretoken, indices in pretoken_sequences.items():
+                    for index1, index2 in zip(indices, indices[1:]):
+                        counts[(index1, index2)] = (counts.get((index1, index2), 0) + count_map[pretoken])
+                pair = max(counts, key=counts.get)  # ty:ignore[no-matching-overload]
+                index1, index2 = pair
+                new_index = 256 + len(special_tokens) + i
+                merges[pair] = new_index
+                vocab[new_index] = (vocab[index1] + vocab[index2])  
 
+            for pretoken in pretoken_sequences:
+                pretoken_sequences[pretoken] = merge(pretoken_sequences[pretoken], pair, new_index)
 
-params = run_train_bpe(string, vocab_size, special_tokens)
-print(params)
+            return vocab, merges
+        return train_bpe_pretokenization(final_count_map, vocab_size - 256 - len(special_tokens))
